9e086bc127e3030abf3c4ddec587fd30b7d2b1f5
Luis Vicente Albarracin <albarracinluisvicente@gmail.com>
2023-03-17 19:52:15 -0500

    ver empleados

---
+++ src/app/lista-empleados/lista-empleados.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Empleado } from '../empleado';
+import { EmpleadoService } from '../empleado.service';
+
+@Component({
+  selector: 'app-lista-empleados',
+  templateUrl: './lista-empleados.component.html',
+  styleUrls: ['./lista-empleados.component.css']
+})
+export class ListaEmpleadosComponent implements OnInit {
+
+  empleados: Empleado[];
+
+  constructor(private empleadoServicio: EmpleadoService){
+
+  }
+
+  ngOnInit(): void {
+    this.obtenerEmpleados();
+  }
+
+  private obtenerEmpleados(){
+    this.empleadoServicio.obtenerListaEmpleados().subscribe(dato => {
+      this.empleados = dato;
+    })
+  }
+
+}
---
+++ src/app/registrar-empleado/registrar-empleado.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrar-empleado',
+  templateUrl: './registrar-empleado.component.html',
+  styleUrls: ['./registrar-empleado.component.css']
+})
+export class RegistrarEmpleadoComponent {
+
+}
